d6277f246a0ae849bf93af8297a33be756242320
Elankumaran98 <siva98kumarane@gmail.com>
2021-07-03 09:34:29 +0530

    started

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export class Navbar extends Component {
+    static propTypes = {
+
+    }
+
+    render() {
+        return (
+            <header>
+                <h1>Kumaran Web</h1>
+                <h2>vanakkam</h2>
+            </header>
+        )
+    }
+}
+
+export default Navbar
